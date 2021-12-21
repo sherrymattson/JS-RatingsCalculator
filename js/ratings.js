f91b76aaf5167b0f829dd8669175cff10954db09
Sherry <sherry.mattson@irwinseating.com>
2021-12-21 12:46:27 -0500

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -1,2 +1,27 @@
 function collect_ratings(){}
-function kevin_k(){}
+const ratings = {'count': 0, 'sum' : 0, 'average': 0};
+
+let rating = 0;
+
+const elements = document.querySelectorAll('.rating');
+
+elements.forEach(element => {
+    rating = parseInt(element.id.replace('star',''));
+    rating.count += parseInt(element.value);
+    rating.sum += parseInt(element.value) * rating;
+});
+
+if (ratings.count !==0) {
+    ratings.average = ratings.sum / ratings.count;
+
+}
+
+return ratings;
+
+}
+document.addEventListener ('change'(), =>) {
+    const ratings = collect_ratings ();
+    document.querySelector(#'average').value = ratings.average.toFixed(2);
+
+
+});
